babb8290a8abc90de7b9660e9c5f858e4134bbd6
alexander <urazaev.alexander@gmail.com>
2020-02-05 18:07:12 +0500

    pre submition

--- monopress/inc/cmb2/image_select_metafield.js
+++
@@ -1,9 +0,0 @@
-window.CMB2 = (function(window, document, $, undefined){
-	'use strict';
-	$('ul.cmb2-image-select-list li input[type="radio"]').click(
-	function(e) {
-	    e.stopPropagation(); // stop the click from bubbling
-	    $(this).closest('ul').find('.cmb2-image-select-selected').removeClass('cmb2-image-select-selected');
-	    $(this).parent().closest('li').addClass('cmb2-image-select-selected');
-	});	
-})(window, document, jQuery);  
